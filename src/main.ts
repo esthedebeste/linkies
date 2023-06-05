595beea53a9e4f08288178391e30a40867bd3ac0
esthedebeste <57283066+esthedebeste@users.noreply.github.com>
2023-06-05 12:18:33 +0000

    Added password protection & a very basic frontend  + added prettier config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,33 @@
 import { serve } from "https://deno.land/std@0.190.0/http/server.ts"
 
+/**
+ * ```
+ * type KV = {
+ * 	 urls: {
+ *  		[name: string]: Shortlink | string
+ *  	}
+ *  }
+ * ```
+ */
 const kv = await Deno.openKv()
+
+interface Shortlink {
+	url: string
+	password?: string
+}
+
+async function getShortlink(name: string): Promise<Shortlink | null> {
+	const { value: url } = await kv.get<Shortlink | string>(["urls", name])
+	if (typeof url === "string") return { url }
+	return url
+}
+
+async function setShortlink(name: string, url: string, password: string | null): Promise<void> {
+	if (password)
+		await kv.set(["urls", name], { url, password })
+	else 
+		await kv.set(["urls", name], url)
+}
 
 const API_DOC = `API:
 https://l.esthe.live?name=NAME&to=URL
@@ -13,6 +40,8 @@ https://l.esthe.live?name=home&to=https://esthe.live/
 
 
 Made with hearts and rainbows! <3`
+
+const DOC_HTML = await Deno.readTextFile(new URL("./doc.html", import.meta.url))
 
 const powered = [
 	"spinning hamster wheels",
@@ -31,20 +60,30 @@ serve(async request => {
 	if (url.pathname === "/") {
 		const name = url.searchParams.get("name")
 		const to = url.searchParams.get("to")
+		const password = url.searchParams.get("password")
 		if (!name || !to)
-			return new Response(API_DOC, {
+			return new Response(DOC_HTML, {
+				headers: {
+					"Content-Type": "text/html; charset=utf-8",
+					"X-Powered-By": powered[Math.floor(Math.random() * powered.length)],
+				},
+			})
+
+		const current = await getShortlink(name)
+		if(current?.password != null && current.password !== password)
+			return new Response(`Forbidden\n\nThe shortlink "${name}" already exists, and you didn't provide the correct password!`, {
+				status: 403,
 				headers: {
 					"Content-Type": "text/plain; charset=utf-8",
 					"X-Powered-By": powered[Math.floor(Math.random() * powered.length)],
 				},
 			})
-
-		await kv.set(["urls", name], to)
+		await setShortlink(name, to, password)
 
 		return new Response(
 			`<!doctype html><html lang=en><title>created</title>Created <a href=${JSON.stringify(
 				`/${encodeURIComponent(name)}`
-			)}>${JSON.stringify(name)}</a> => ${JSON.stringify(to)}`,
+			)}>${JSON.stringify(name)}</a> => ${JSON.stringify(to)} with password ${JSON.stringify(password)}`,
 			{
 				status: 201,
 				headers: {
@@ -56,13 +95,13 @@ serve(async request => {
 	}
 
 	const path = decodeURIComponent(url.pathname.slice(1)) + url.search
-	const to = await kv.get<string>(["urls", path])
+	const shortlink = await getShortlink(path)
 
-	if (to.value != null)
-		return new Response(`Found! Redirecting to ${to.value}`, {
+	if (shortlink != null)
+		return new Response(`Found! Redirecting to ${shortlink.url}`, {
 			status: 307,
 			headers: {
-				Location: to.value,
+				Location: shortlink.url,
 				"Content-Type": "text/plain; charset=utf-8",
 				"X-Powered-By": powered[Math.floor(Math.random() * powered.length)],
 			},
